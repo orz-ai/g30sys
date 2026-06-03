199f5beeef80138a5f701d3f17910383707c9ae5
Gang <deepincode@qq.com>
2026-06-04 03:07:06 +0800

    feat: [harib03e] 中断处理程序

---
+++ day06/harib03e/README.md
@@ -0,0 +1,14 @@
+# 1. 更新的文件
+1. int.c
+2. naskfunc.nas
+3. dsctbl.c
+4. bootpack.h
+5. bootpack.c
+
+# 2. 启动
+1. 直接执行 `make run`
+
+# 3. 知识点
+
+# 4. 总结
+1. init_pic
---
+++ day06/harib03e/bootpack.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "bootpack.h"
+
+void HariMain(void)
+{
+	struct BOOTINFO *binfo = (struct BOOTINFO *)0x0ff0;
+	char s[40], mcursor[256];
+
+	int mx, my;
+
+	init_gdtidt();
+	init_pic();
+	io_sti(); /* IDT/PIC的初始化已经完成，放开CPU的中断 */
+
+	init_palette();
+	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+
+	mx = (binfo->scrnx - 16) / 2;
+	my = (binfo->scrny - 28 - 16) / 2;
+
+	init_mouse_cursor8(mcursor, COL8_008484);
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+
+	sprintf(s, "(%d, %d)", mx, my);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+	io_out8(PIC0_IMR, 0xf9); /* 11111001 PIC1以外全部禁止 */
+	io_out8(PIC1_IMR, 0xef); /* 11101111 禁止鼠标以外的中断 */
+
+	for (;;)
+	{
+		io_hlt();
+	}
+}
